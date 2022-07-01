f21417330f7d8ddd453560a5aac0e36d9be29f70
malpin <returnpanda@outlook.com>
2022-07-01 16:43:06 +0800

    添加了雪花算法生成id

---
+++ tool/snowflake.go
@@ -0,0 +1,24 @@
+package tool
+
+//提供了一个非常简单的雪花ID生成器和解析器。
+import (
+	"github.com/bwmarrin/snowflake"
+	"time"
+)
+
+var snowFlakeID *snowflake.Node
+
+func Init(startTime string, machineID int64) (err error) {
+	var st time.Time
+	st, err = time.Parse("2006-01-02", startTime)
+	if err != nil {
+		return
+	}
+	snowflake.Epoch = st.UnixNano() / 1000000
+	snowFlakeID, err = snowflake.NewNode(machineID)
+	return
+}
+
+func GenID() int64 {
+	return snowFlakeID.Generate().Int64()
+}
